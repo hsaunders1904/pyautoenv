c4249500d647d88ff817a9538eb796ad7c2b1d09
Harry Saunders <33317174+hsaunders1904@users.noreply.github.com>
2023-03-27 00:02:44 +0100

    Fix typing for Python 3.8

--- autopyenv.py
+++ autopyenv.py
@@ -7,7 +7,7 @@ import os
 import subprocess
 from dataclasses import dataclass
 from pathlib import Path
-from typing import TextIO, Union
+from typing import List, TextIO, Union
 
 
 @dataclass
@@ -33,7 +33,7 @@ class Env:
     env_type: EnvType
 
 
-def main(sys_args: list[str], stdout: TextIO) -> int:
+def main(sys_args: List[str], stdout: TextIO) -> int:
     """Activate environment if it exists in the given directory."""
     args = parse_args(sys_args)
     if not args.directory.is_dir():
@@ -52,7 +52,7 @@ def main(sys_args: list[str], stdout: TextIO) -> int:
     return 0
 
 
-def parse_args(sys_args: list[str]) -> CliArgs:
+def parse_args(sys_args: List[str]) -> CliArgs:
     """Parse the sequence of command line arguments."""
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument(
