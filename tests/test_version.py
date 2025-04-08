df7b4263492ef06ac01221c564c86234679d0eba
Harry Saunders <33317174+hsaunders1904@users.noreply.github.com>
2025-04-08 13:10:46 +0100

    Replace poetry with uv

--- tests/test_version.py
+++ tests/test_version.py
@@ -1,9 +1,7 @@
 """Tests for the versioning of pyautoenv."""
 
 import re
-from pathlib import Path
 
-import toml
 from packaging.version import VERSION_PATTERN
 
 import pyautoenv
@@ -13,11 +11,3 @@ def test_version_is_pep440_compliant():
     pattern = re.compile(VERSION_PATTERN, flags=re.IGNORECASE | re.VERBOSE)
 
     assert pattern.match(pyautoenv.__version__)
-
-
-def test_version_in_pyproject_eq_to_module_version():
-    pyproject_file = Path(__file__).parent.parent / "pyproject.toml"
-    pyproject = toml.loads(pyproject_file.read_text())
-    pyproject_version = pyproject["tool"]["poetry"]["version"]
-
-    assert pyautoenv.__version__ == pyproject_version
