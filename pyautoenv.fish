c9f0b81c228451811dbfa9c2356bdf342a466b4b
Harry Saunders <33317174+hsaunders1904@users.noreply.github.com>
2025-04-19 08:28:19 +0100

    Add debug logging

--- pyautoenv.fish
+++ pyautoenv.fish
@@ -33,12 +33,16 @@ function _pyautoenv_activate \
     end
     set _pyautoenv_py "$_pyautoenv_path/pyautoenv.py"
     if test -f "$_pyautoenv_py"
-        eval (python3 "$_pyautoenv_py" --fish)
+        if test "$PYAUTOENV_DEBUG" != "0"
+            eval (python3 "$_pyautoenv_py" --fish)
+        else
+            eval (python3 -O "$_pyautoenv_py" --fish)
+        end
     end
 end
 
 function _pyautoenv_version --description 'Print pyautoenv version'
-    python3 "$_pyautoenv_path/pyautoenv.py" --version
+    python3 -O "$_pyautoenv_path/pyautoenv.py" --version
 end
 
 emit _pyautoenv_fish_init
